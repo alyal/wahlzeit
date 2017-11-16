a2230f7bbead3b5165beb33681c783b43f986709
Alexander Aly <alexander.aly@t-online.de>
2017-11-16 19:07:15 +0100

    Changes in PhotoManager to get intance back to run

--- src/main/java/org/wahlzeit/model/BuildingsPhotoManager.java
+++ src/main/java/org/wahlzeit/model/BuildingsPhotoManager.java
@@ -12,48 +12,6 @@ public class BuildingsPhotoManager extends PhotoManager {
 	 */
 	protected static final BuildingsPhotoManager instance = new BuildingsPhotoManager();
 	
-	private static final Logger log = Logger.getLogger(BuildingsPhotoManager.class.getName());
-
-	/**
-	 *
-	 */
-	protected BuildingsPhotoManager() {
-		photoTagCollector = BuildingsPhotoFactory.getInstance().createPhotoTagCollector();
-	}
-
-	/**
-	 *
-	 */
-	public Photo getPhotoFromId(PhotoId id) {
-		if (id == null) {
-			return null;
-		}
-
-		Photo result = doGetPhotoFromId(id);
-
-		if (result == null) {
-			result = BuildingsPhotoFactory.getInstance().loadPhoto(id);
-			if (result != null) {
-				doAddPhoto(result);
-			}
-		}
-
-		return result;
-	}
-
-	/**
-	 *
-	 */
-	public static final BuildingsPhotoManager getInstance() {
-		return instance;
-	}
-
-	/**
-	 * 
-	 * 
-	 */
-	protected Logger getLogger() {
-		return log;
-	}
+	
 
 }
