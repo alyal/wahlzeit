0c5bd36f4c7d474f8632e59db30476786e0d40b8
alyal <alexander.aly@t-online.de>
2017-12-31 13:07:39 +0100

    Added Java annotation Type for Design Pattern as presented in lecture

---
+++ src/main/java/org/wahlzeit/model/DesignPattern.java
@@ -0,0 +1,11 @@
+package org.wahlzeit.model;
+
+/**
+ * Java Annotation Type for Design Patterns (as on lectures slides C08 p.32)
+ * 
+ */
+public @interface DesignPattern {
+	String name();
+	String[] participants();
+
+}
